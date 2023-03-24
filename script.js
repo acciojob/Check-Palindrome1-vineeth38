7389f21c71d76ac186b5fb05880b0a641c7a138f
vineeth38 <119692597+vineeth38@users.noreply.github.com>
2023-03-24 09:56:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-
+let j = str.length -1;
+      for( let i = 0 ; i < j/2 ;i++)
+      {
+        let x = str[i] ;//forward character
+        let y = str[j-i];//backward character
+        if( x != y)
+        {
+          // return false if string not match
+          return false;
+        }
+      }
+      /// return true if string is palindrome
+      return true;
 }
 module.exports = palindrome
