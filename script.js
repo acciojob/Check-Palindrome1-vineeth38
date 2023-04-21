ffce4c25eea6d77497870131b6a34f6d2a7648be
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-04-21 10:59:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // complete the given function
 
 function palindrome(str){
-let j = str.length;
+let j = str.length-1;
       for( let i = 0 ; i < j/2 ;i++)
       {
         let x = str[i] ;//forward character
